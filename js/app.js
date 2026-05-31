b7a483bdf1d7ef02e122fabde31f875c132d94f3
sollykingjr <maxcsolomon18@gmail.com>
2026-05-31 17:21:25 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,6 @@ function setSection(s) {
   window.scrollTo(0, 0);
 
   if (isWatch) {
-    tab = 'watch';
     document.getElementById('cntlbl').textContent = '';
     if (!watchlistLoaded) {
       loadWatchlist();
@@ -36,9 +35,6 @@ function setSection(s) {
       renderWatchlist();
     }
   } else {
-    tab = 'all';
-    document.querySelectorAll('.tab').forEach(x => x.classList.remove('on'));
-    document.querySelector('.tab[data-t="all"]').classList.add('on');
     render();
   }
 
